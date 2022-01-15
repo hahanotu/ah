f6531424130aecacb7dbbdd0bf5050b9f817781d
hahanotu <76503171+hahanotu@users.noreply.github.com>
2022-01-15 14:42:50 -0800

    Update normalhb.lua

--- flexgui/normalhb.lua
+++ flexgui/normalhb.lua
@@ -1,20 +1,19 @@
-
 _G.Resized = false
 
 local Player = game:GetService('Players').LocalPlayer
 local Mouse = Player:GetMouse()
 
 Mouse.KeyDown:Connect(function(Key)
-        if Key == 'b' then
+    if Key == 'g' then
         _G.Resized = not _G.Resized
         if _G.Resized == true then
-            Player.Character['LeftHand'].Size = Vector3.new(4, 10, 4)
-            Player.Character['RightHand'].Size = Vector3.new(4, 10, 4)
+            Player.Character['LeftHand'].Size = Vector3.new(3, 10, 3)
+            Player.Character['RightHand'].Size = Vector3.new(3, 10, 3)
         Player.Character['LeftHand'].Transparency = 0.5
         Player.Character['RightHand'].Transparency = 0.5
         elseif _G.Resized == false then
-            Player.Character['LeftHand'].Size = Vector3.new(0.69334, 1.10182, 0.744331)
-            Player.Character['RightHand'].Size = Vector3.new(0.69334, 1.10182, 0.744331)
+            Player.Character['LeftHand'].Size = Vector3.new(1,5, 2, 1)
+            Player.Character['RightHand'].Size = Vector3.new(1.5, 2, 1)
         Player.Character['LeftHand'].Transparency = 0
         Player.Character['RightHand'].Transparency = 0
         end
