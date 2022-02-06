0808766bc8f02333ddf7391f9c8b3fa8e5efb154
hahanotu <76503171+hahanotu@users.noreply.github.com>
2022-02-05 23:14:05 -0800

    Create naaaboo.lua

---
+++ flexgui/naaaboo.lua
@@ -0,0 +1,41 @@
+local b = is_synapse_function
+local q = game.PlaceId
+local bloxfruit = 2753915549
+local rbw6 = 2621503555
+local lol = q == 2753915549
+local lol2 = q ==  2621503555
+local rbw1 = "https://raw.githubusercontent.com/hahanotu/ah/main/flexgui/safdsafdsfds.lua"
+local MarketplaceService = game:GetService("MarketplaceService")
+local a = MarketplaceService:GetProductInfo(game.PlaceId)["Name"]
+local blox2 = "https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua"
+
+local function msg1()
+    messagebox(a, "Script executed!", 0)
+end
+
+
+local function msg2()
+    messagebox(a, "Game not Supported!", 0)
+end
+
+
+if q == rbw6 then
+    loadstring(game:HttpGet(rbw1, true))()
+
+    if b then
+        msg1()
+    end
+
+
+elseif q == bloxfruit then
+    loadstring(game:HttpGet(blox2, true))()
+    if b then
+        msg1()
+    end
+
+
+else
+    if b then
+        msg2()
+    end
+end
